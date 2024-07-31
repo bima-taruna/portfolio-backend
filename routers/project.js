4c9d987ba88c239eb8c4e0a62a1c5e833bc36daa
bima-taruna <bimataruna21@gmail.com>
2024-07-31 17:48:31 +0700

    change dotenv

--- routers/project.js
+++ routers/project.js
@@ -1,4 +1,4 @@
-require("dotenv/config");
+require("dotenv").config();
 const express = require("express");
 const { Project } = require("../models/project");
 const upload = require("../config/multer");
--- routers/users.js
+++ routers/users.js
@@ -1,4 +1,4 @@
-require("dotenv/config");
+require("dotenv").config();
 const express = require("express");
 const { User } = require("../models/user");
 const router = express.Router();
